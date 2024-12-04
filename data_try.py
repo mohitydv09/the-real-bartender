0d3e1356a9a471b3f3d88156ce57550dd1431766
Mohit <mohitydv09@gmail.com>
2024-12-04 01:52:26 -0600

    Dataloader and other filed changed to incorporate three cameras

--- data_try.py
+++ data_try.py
@@ -1,19 +1,29 @@
-import zarr
-import cv2
-import numpy as np
+#!/usr/bin/env python
 
-path = 'pusht/pusht_cchi_v7_replay.zarr'
+import rospy
+from message_filters import ApproximateTimeSynchronizer, Subscriber
+from std_msgs.msg import Float32MultiArray, Bool
 
-dataset = zarr.open(path, mode='r')
+# Callback to process synchronized messages
+def sync_callback(msg1, msg2):
+    rospy.loginfo("Synchronized messages received")
+    pub_sync_1.publish(msg1)
+    pub_sync_2.publish(msg2)
 
-print(dataset.tree())
+if __name__ == '__main__':
+    rospy.init_node('synchronizer_node')
 
-# print(dataset['data']['img'][0])
+    # Subscribers for the topics to be synchronized
+    sub1 = Subscriber('/Force/force_ctl', Float32MultiArray)
+    sub2 = Subscriber('/Force/policy', Float32MultiArray)
 
-print(np.max(dataset['data']['img'][0]))
-print(np.min(dataset['data']['img'][0]))
+    # Publishers for synchronized topics
+    pub_sync_1 = rospy.Publisher('/sync/force_ctl', Float32MultiArray, queue_size=10)
+    pub_sync_2 = rospy.Publisher('/sync/policy', Float32MultiArray, queue_size=10)
 
-# cv2.imshow('image', dataset['data']['img'][0]/255)
-# cv2.waitKey(0)
-print(dataset['meta']['episode_ends'].shape)
-print(dataset['meta']['episode_ends'][0:206])
+    # Synchronizer with a small time slop
+    sync = ApproximateTimeSynchronizer([sub1, sub2], queue_size=10, slop=0.05)
+    sync.registerCallback(sync_callback)
+
+    rospy.loginfo("Synchronization node started")
+    rospy.spin()
