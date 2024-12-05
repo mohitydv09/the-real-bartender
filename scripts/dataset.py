ca7744a6dc98d78711462b0576b3cc46d229b878
Mohit <mohitydv09@gmail.com>
2024-12-04 18:11:20 -0600

    organised the folders

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -81,7 +81,7 @@ def unnormalize_data(ndata, stats):
     return data
 
 # dataset
-class PushTImageDataset(torch.utils.data.Dataset):
+class BartenderDataset(torch.utils.data.Dataset):
     def __init__(self,
                  dataset_path: str,
                  pred_horizon: int,
