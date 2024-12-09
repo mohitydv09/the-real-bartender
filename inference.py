e704f1afeaebf5e49b72e80d92a25fc4e8fcef88
Mohit <mohitydv09@gmail.com>
2024-12-09 17:35:09 -0600

    Inference Script updated

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+from typing import List
 import os
 import sys
 import time
@@ -37,11 +38,17 @@ from Spark.TeleopMethods.UR.gripper import RobotiqGripper
 ROBOT_SPEED = 0.3           ## Speed of the robot in m/s
 ROBOT_ACCELERATION = 0.3    ## Acceleration of the robot in m/s^2
 ROBOT_BLEND = 0.001          ## Blend value for the robot
-ROBOT_VELOCITY_SCALE = 1.0  
+ROBOT_VELOCITY_SCALE = 0.5  
+UR_TIME = 0.001
+UR_LOOKAHEAD_TIME = 0.05
+UR_GAIN = 200
+
 
 LIGHTNING_HOME = [-3.2225098609924316, -0.6799390912055969, -2.337024450302124, -1.1205544471740723, 1.7932333946228027, 1.2651606798171997]
 THUNDER_HOME = [-2.82061505317688, -2.5057523250579834, 2.3684492111206055, -1.921329140663147, -1.6988776922225952, -1.2206653356552124]
 
+LIGHTNING_GRIPPER_OPEN = 0
+THUNDER_GRIPPER_OPEN = 0
 
 def load_config(config_path):
     with open(config_path, 'r') as f:
@@ -79,7 +86,6 @@ def load_network(config, device):
 
     return networks
 
-
 def normalize_data(data, stats):
     # nomalize to [0,1]
     ndata = (data - stats['min']) / (stats['max'] - stats['min'])
@@ -87,12 +93,10 @@ def normalize_data(data, stats):
     ndata = ndata * 2 - 1
     return ndata
 
-
 def unnormalize_data(ndata, stats):
     ndata = (ndata + 1) / 2
     data = ndata * (stats['max'] - stats['min']) + stats['min']
     return data
-
 
 def get_observations(observation_object, config, stats):
     '''
@@ -120,11 +124,6 @@ def get_observations(observation_object, config, stats):
     img_wrist_lightning = obs_dict['Images']['img_wrist_lightning']
     agent_pos = obs_dict['agent_state']
 
-    print("Agent Pos: ", agent_pos.shape)
-    print("img_front: ", img_front.shape)
-    print("img_wrist_thunder: ", img_wrist_thunder.shape)
-    print("img_wrist_lightning: ", img_wrist_lightning.shape)
-
     # Normalize the images
     img_front = img_front.astype(np.float32) / 255.0
     img_wrist_thunder = img_wrist_thunder.astype(np.float32) / 255.0
@@ -149,7 +148,6 @@ def get_observations(observation_object, config, stats):
         'img_wrist_lightning': img_wrist_lightning,
         'agent_pos': agent_pos,
     }
-
 
 
 def run_inference(obs_dict, networks, noise_scheduler, stats, config, device):
@@ -201,7 +199,7 @@ def run_inference(obs_dict, networks, noise_scheduler, stats, config, device):
     end = start + config['action_horizon']
     action = naction[start:end, :]
 
-    return action
+    return action.tolist()
 
 def init_robot():
     '''
@@ -221,146 +219,42 @@ def init_robot():
     lightning_io.setSpeedSlider(ROBOT_VELOCITY_SCALE)
     thunder_io.setSpeedSlider(ROBOT_VELOCITY_SCALE)
 
-    ## Make ROS publishers for the robot
-    # pubs = dict()
-    # for arm in arms:
-    #     pubs[arm+"_q"] = rospy.Publisher(f"/{arm.lower()}_q", Float32MultiArray, queue_size=10)
-    #     pubs[arm+"_gripper"] = rospy.Publisher(f"/{arm.lower()}_gripper", Float32, queue_size=10)
-
     for arm in arms:
         URs.init_dashboard(arm)
         URs.init_arm(arm, enable_control=enable_control)
     return URs
 
-def perform_action(URs: UR, action: np.ndarray) -> None:
+def perform_action(URs: UR, action: List[List]) -> None:
     """
     Perform the current action as given be the model.
     Action is a numpy array of shape (action_horizon, action_dim).
     """
-    movement_params = np.array([ROBOT_SPEED, ROBOT_ACCELERATION, ROBOT_BLEND])
-    movement_params = np.tile(movement_params, (action.shape[0], 1))
-    lightning_actions = np.concatenate((action[:, :6], movement_params), axis=1)
-    lightning_actions[-1,-1] = 0
-    thunder_actions = np.concatenate((action[:, 7:13], movement_params), axis=1)
-    thunder_actions[-1,-1] = 0
+    global LIGHTNING_GRIPPER_OPEN
+    global THUNDER_GRIPPER_OPEN
+    lightning_actions = action[0:6]
+    lightning_gripper = action[6]
+    thunder_actions = action[7:13]
+    thunder_gripper = action[13]
 
-    lightning_actions = lightning_actions.tolist()
-    thunder_actions = thunder_actions.tolist()
-    lightning_grippers = action[:, 6].tolist()
-    thunder_grippers = action[:, 13].tolist()
+    URs.servoJ("Lightning", (lightning_actions, 0.0, 0.0, UR_TIME, UR_LOOKAHEAD_TIME, UR_GAIN))
+    URs.servoJ("Thunder", (thunder_actions, 0.0, 0.0, UR_TIME, UR_LOOKAHEAD_TIME, UR_GAIN))
 
-    rate = rospy.rate(10)
+    if lightning_gripper > 50:
+        LIGHTNING_GRIPPER_OPEN = 1
+    if thunder_gripper > 50:
+        THUNDER_GRIPPER_OPEN = 1
 
-    rate.sleep()
+    if LIGHTNING_GRIPPER_OPEN:
+        URs.get_gripper("Lightning").set(int(255))
+    else:
+        URs.get_gripper("Lightning").set(int(0))
+    if THUNDER_GRIPPER_OPEN:
+        URs.get_gripper("Thunder").set(int(255))
+    else:
+        URs.get_gripper("Thunder").set(int(0))
+    # URs.get_gripper("Thunder").set(int(thunder_gripper))
 
-    URs.moveJ("Lightning", lightning_actions, asynchronous=True)
-    URs.moveJ("Thunder", thunder_actions, asynchronous=False)
 
-# class ObservationSubscriber:
-#     def __init__(self, obs_horizon):
-#         self.node_name = 'image_subscriber'
-#         rospy.init_node(self.node_name, anonymous=True)
-
-#         # Define the observation horizon
-#         self.obs_horizon = obs_horizon
-
-#         # Deques to store the last 'n' observations for each image
-#         self.img_front_history = deque(maxlen=obs_horizon)
-#         self.img_wrist_thunder_history = deque(maxlen=obs_horizon)
-#         self.img_wrist_lightning_history = deque(maxlen=obs_horizon)
-
-#         # Deques for agent states
-#         self.lightning_angle_history = deque(maxlen=obs_horizon)
-#         self.thunder_angle_history = deque(maxlen=obs_horizon)
-#         self.lightning_gripper_history = deque(maxlen=obs_horizon)
-#         self.thunder_gripper_history = deque(maxlen=obs_horizon)
-
-#         # Temporary storage for incoming messages
-#         self.latest_img_front = None
-#         self.latest_img_wrist_thunder = None
-#         self.latest_img_wrist_lightning = None
-#         self.latest_lightning_angle = None
-#         self.latest_thunder_angle = None
-#         self.latest_lightning_gripper = None
-#         self.latest_thunder_gripper = None
-
-#         # Subscribers
-#         rospy.Subscriber('/cameras/rgb/thunder/wrist', Image, self.thunder_wrist)
-#         rospy.Subscriber('/cameras/rgb/lightning/wrist', Image, self.lightning_wrist)
-#         rospy.Subscriber('cameras/rgb/both/front', Image, self.both_front)
-#         rospy.Subscriber('/lightning_q', Float32MultiArray, self.lightning_q)
-#         rospy.Subscriber('/thunder_q', Float32MultiArray, self.thunder_q)
-#         rospy.Subscriber('/thunder_gripper', Float32, self.thunder_gripper)
-#         rospy.Subscriber('/lightning_gripper', Float32, self.lightning_gripper)
-
-#     def thunder_wrist(self, msg):
-#         # Store the latest wrist image for thunder
-#         self.latest_img_wrist_thunder = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-
-#         # Append all queued data to history when the thunder wrist image arrives
-#         if self.latest_img_wrist_thunder is not None:
-#             self.img_wrist_thunder_history.append(self.latest_img_wrist_thunder)
-#             if self.latest_img_front is not None:
-#                 self.img_front_history.append(self.latest_img_front)
-#             if self.latest_img_wrist_lightning is not None:
-#                 self.img_wrist_lightning_history.append(self.latest_img_wrist_lightning)
-#             if self.latest_lightning_angle is not None:
-#                 self.lightning_angle_history.append(self.latest_lightning_angle)
-#             if self.latest_thunder_angle is not None:
-#                 self.thunder_angle_history.append(self.latest_thunder_angle)
-#             if self.latest_lightning_gripper is not None:
-#                 self.lightning_gripper_history.append(self.latest_lightning_gripper)
-#             if self.latest_thunder_gripper is not None:
-#                 self.thunder_gripper_history.append(self.latest_thunder_gripper)
-
-#     def lightning_wrist(self, msg):
-#         # Store the latest lightning wrist image
-#         self.latest_img_wrist_lightning = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-
-#     def both_front(self, msg):
-#         # Store the latest front image
-#         self.latest_img_front = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
-
-#     def lightning_q(self, msg):
-#         # Store the latest lightning angle (size 6)
-#         self.latest_lightning_angle = np.asarray(msg.data)
-
-#     def thunder_q(self, msg):
-#         # Store the latest thunder angle (size 6)
-#         self.latest_thunder_angle = np.asarray(msg.data)
-
-#     def lightning_gripper(self, msg):
-#         # Store the latest lightning gripper state (scalar)
-#         self.latest_lightning_gripper = np.asarray(msg.data)
-
-#     def thunder_gripper(self, msg):
-#         # Store the latest thunder gripper state (scalar)
-#         self.latest_thunder_gripper = np.asarray(msg.data)
-
-#     def get_last_n_observations(self):
-#         """Returns the last 'n' observations structured as per the requirement."""
-#         images = {
-#             'img_front': np.array(self.img_front_history),
-#             'img_wrist_thunder': np.array(self.img_wrist_thunder_history),
-#             'img_wrist_lightning': np.array(self.img_wrist_lightning_history),
-#         }
-
-#         # Agent state is a concatenation of:
-#         # - lightning angle (6,)
-#         # - lightning gripper (1,)
-#         # - thunder angle (6,)
-#         # - thunder gripper (1,)
-#         agent_state = np.column_stack((
-#             np.array(self.lightning_angle_history),        # (obs_horizon, 6)
-#             np.array(self.lightning_gripper_history),      # (obs_horizon, 1)
-#             np.array(self.thunder_angle_history),         # (obs_horizon, 6)
-#             np.array(self.thunder_gripper_history)        # (obs_horizon, 1)
-#         ))
-
-#         return {
-#             'Images': images,  # (obs_horizon, h, w, c)
-#             'agent_state': agent_state  # (obs_horizon, 14) - concatenation of the agent's states
-#         }
 
 def main():
     ## Load Config and set device
@@ -380,29 +274,34 @@ def main():
 
     ## Subscribe to the ROS topics
     observation_subscriber = ObservationSubscriber(obs_horizon=config['observation_horizon'])
-    rate = rospy.Rate(10) # 10hz
+    time.sleep(2)
 
-    rospy.sleep(2)
     ## Initialize the robot
     URs = init_robot()
 
-    ## Move robot to home position
-    # URs.moveJ("Lightning", LIGHTNING_HOME, asynchronous=True)
+    ## Move the robot to the home position
+    URs.moveJ("Thunder", THUNDER_HOME, asynchronous=False)
+    URs.moveJ("Lightning", LIGHTNING_HOME, asynchronous=False)
+
     input("Press Enter to Move Robot to Home Position...")
+    action_buffer = deque(maxlen=config['action_horizon'])
     
-    while not rospy.is_shutdown():
-        obs_dict = get_observations(observation_subscriber, config, stats)
-        action = run_inference(obs_dict, networks, noise_scheduler, stats, config, device)
-        # print("Action: \n", action)
+    RUNNING_TIME = 0.4
+    try:
+        while True:
+            start_time = time.time()
+            if (len(action_buffer) == 0):
+                obs_dict = get_observations(observation_subscriber, config, stats)
+                action = run_inference(obs_dict, networks, noise_scheduler, stats, config, device)
+                action_buffer.extend(action)
+            ## Move the robot to the first action in the buffer
+            next_action = action_buffer.popleft()
+            perform_action(URs, next_action)
+            time.sleep(max(0, RUNNING_TIME - (time.time() - start_time)))
+    except KeyboardInterrupt:
+        print("Exiting the program...")
+        exit()
 
-        # input("Press Enter to Mvoe Robot...")
-        perform_action(URs, action)
-
-        ## Break Condition 
-        ## TODO: Implement the break condition
-
-        rate.sleep()
-
-
+            
 if __name__ == '__main__':
     main()
